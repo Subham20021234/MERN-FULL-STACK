494d6f53b07bbefd0326a7c81051fa1b9b333834
Subham Suvendu Das <139585111+Subham20021234@users.noreply.github.com>
2025-07-02 10:12:00 +0530

    Add files via upload

---
+++ 27-02-205/array_adavnce_loops.js
@@ -0,0 +1,32 @@
+// let fruits = ["mango","Banana","apple"]
+
+// fruits.forEach((Element, index, array) => {
+//     console.log(Element.toUpperCase());
+// });
+
+
+
+
+// const arr2 = [45, 33, 78, 66, 78, 15];
+
+// const result = arr2.filter((element, index, array) => {
+//     // return element % 2 == 0;
+//     arr2.pop()
+//     return true
+// })
+
+// const arr1 = 
+// console.log(arr1*arr2);
+
+
+
+// const arr = [[1,[2,3],4],5]
+// const flat = arr.reduce((acc,curr)=> {
+//     return acc.concat(curr).flat()},[]
+// );
+
+
+// console.log(flat);
+
+
+
---
+++ 27-02-205/regex_practice.js
@@ -0,0 +1,52 @@
+// email validation 
+// const checker = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// console.log(checker.test("subham@examle.com")); 
+// console.log(checker.test("subham@.com"));      
+
+
+// string validation conatining only number 
+// const string_checker = /^\d+$/;
+// console.log(string_checker.test("123456"));  
+// console.log(string_checker.test("123abc"));  
+  
+
+
+// postal code
+// const postal_code_validator = /^\d{5}$/;
+// console.log(postal_code_validator.test("56001"));   
+// console.log(postal_code_validator.test("560012")); 
+// console.log(postal_code_validator.test("5600a"));  
+
+// valid mobile number
+// const valid_num = /^[6-9]\d{9}$/;
+// console.log(valid_num.test("9876543210"));  
+// console.log(valid_num.test("5123456789"));  
+
+// password validation
+// const pass_valid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/;
+// console.log(pass_valid.test("Pass123"));   
+// console.log(pass_valid.test("password"));   
+// console.log(pass_valid.test("172t766127"));
+
+
+// starts with hellow 
+// const hlw = /^Hello/g;
+// console.log(hlw.test("Hello Subham"));  
+// console.log(hlw.test("hello Subham"));  
+// console.log(hlw.test("Hi Hello"));  
+
+
+// ends with .com
+// const com = /\.com$/;
+// console.log(com.test("google.com"));   
+// console.log(com.test("google.in"));
+
+
+ 
+
+
+// remove white spaces
+// const white = "Bankai    Katen     Kyokotsu     Karamatsu             Shinju.";
+// const nospace = white.replace(/\s+/g, ' ');
+// console.log(nospace);  
+
